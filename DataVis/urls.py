3c3d66ab711cc5aac43e40b622e5990782c2b510
youssefoukhouya <oukhouyayoussef1@gmail.com>
2020-01-22 20:18:30 +0100

    Fixed an error about json format and added a new route to the urls file
    So the problem was that json didn't accept dictionary keys to be date time object so using the strftime method,I converted the datetime object to string and passed it as key.
    As for the urls file i added a route that will contain data brought from the database in a json format using the jsonify method of the flask package.

--- DataVis/urls.py
+++ DataVis/urls.py
@@ -3,7 +3,7 @@ this file is for attributing urls to actions
 """
 
 from DataVis import app
-from flask import render_template
+from flask import render_template, jsonify
 from DataVis.metier.user import User
 
 
@@ -11,3 +11,8 @@ from DataVis.metier.user import User
 @app.route('/SO10')
 def index():
     return render_template('so10.html', list_rep=User().get_reputation_year(2))
+
+
+@app.route("/SO10/data")
+def reputation_year():
+    return jsonify(User().get_reputation_year(2))
